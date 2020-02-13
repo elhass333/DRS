69be7cacbd0b8f57fdac13c812dad44c87f86a6f
jesus alejandro Ramirez Martinez <ing.jesusalejandromtz@gmail.com>
2020-02-13 11:31:32 -0600

    final

--- DRSecurity/Form1.cs
+++ DRSecurity/Form1.cs
@@ -75,6 +75,7 @@ namespace DRSecurity
                 using( testEntities5 db = new testEntities5() )
                 {
                     db.Delete_Usr(CurpAux);
+
                 }
                     Refresh();
             }
